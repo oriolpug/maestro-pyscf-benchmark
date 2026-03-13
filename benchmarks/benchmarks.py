03cab3f4efa2ad42e5e755a36fde643548393206
uri <oriol@qoroquantum.net>
2026-03-13 09:05:46 +0100

    mps bond dim

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -276,7 +276,8 @@ def _run_qiskit_vqe(hf, norb, nelec, ansatz_type="PUCCD", timeout_s=0, mps_bond_
         else:
             from qiskit_aer.primitives import EstimatorV2 as AerEstimator
             aer_est = AerEstimator()
-            aer_est.set_options(method="matrix_product_state", matrix_product_state_max_bond_dimension=mps_bond_dim)
+            aer_est.options.backend_options["method"] = "matrix_product_state"
+            aer_est.options.backend_options["matrix_product_state_max_bond_dimension"] = mps_bond_dim
             params_order = list(ansatz.parameters)
 
             def _energy(params):
